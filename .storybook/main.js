d049f316c0fc600d3285ed7f26a4fe26dd188f41
halasalhab <halasalhab0@gmail.com>
2023-12-18 13:06:19 +0200

    fixing the storybook and mui setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ const config = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-themes"
   ],
   framework: {
     name: "@storybook/react-webpack5",
@@ -21,22 +20,5 @@ const config = {
     autodocs: "tag",
   },
   staticDirs: ["..\\public"],
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      // Speeds up Storybook build time
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-      // Makes union prop types like variant and size appear as select controls
-      shouldExtractLiteralValuesFromEnum: true,
-      // Makes string and boolean types that can be undefined appear as inputs and switches
-      shouldRemoveUndefinedFromOptional: true,
-      // Filter out third-party props from node_modules except @mui packages
-      propFilter: (prop) =>
-        prop.parent ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName) : true,
-    },
-  },
 };
 export default config;
